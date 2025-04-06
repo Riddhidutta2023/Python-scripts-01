9cba840a7642443f1ba8894a59da145a5aa910c7
Riddhi Pratim Dutta <riddhipdutta@gmail.com>
2025-04-06 21:48:22 +0530

    Create last-digit.py
    Prints the last three digit of a number

---
+++ last-digit.py
@@ -0,0 +1,4 @@
+# This program takes an eight-digit number as input and prints the last three digits of that number.
+
+number_input=input(str('Type a eight digit number: '))
+print(number_input[-3:])
