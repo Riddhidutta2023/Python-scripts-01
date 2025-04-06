71f131afda1587b4eadbbd6f395a1edfe2295e18
Riddhi Pratim Dutta <riddhipdutta@gmail.com>
2025-04-06 21:55:55 +0530

    Create number-conversion.py
    This program takes a number as input and prints its binary, octal, and hexadecimal representations.

---
+++ number-conversion.py
@@ -0,0 +1,6 @@
+# This program takes a number as input and prints its binary, octal, and hexadecimal representations.
+
+number_input = int(input("type a number: "))
+print(bin(number_input))
+print(oct(number_input))
+print(hex(number_input))
