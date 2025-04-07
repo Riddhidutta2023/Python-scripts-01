41626b21c56c3241c6c95529eabcac2f499d7416
Riddhi Pratim Dutta <riddhipdutta@gmail.com>
2025-04-07 19:49:52 +0530

    Create email-extractor.py
    This program extracts name and domain from user supplied email address

---
+++ email-extractor.py
@@ -0,0 +1,17 @@
+# This program extracts the name from a supplied email
+
+
+email_supplied=input('Write down the email address:  \n')
+print(f"You wrote {email_supplied}\n")
+
+if '@'in email_supplied:
+    index_of_delimiter = email_supplied.index('@')
+    name=email_supplied[0:index_of_delimiter]
+    email=email_supplied[index_of_delimiter+1:]
+else:
+    print('The email supplied is invalid')
+
+print(f"name:{name},domain:{email}")
+
+
+
